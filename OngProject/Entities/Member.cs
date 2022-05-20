6f8ff1abc5742466a81202de4bdeb5530115590e
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-05-20 12:29:10 -0300

    Model, DbContext & Migration

---
+++ OngProject/Entities/Member.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class Member
+    {
+        [Key]
+        public int MembersID {set;get;}
+        public string Name {set;get;}
+        public string? FacebookUrl {set;get;}
+        public string? InstagramUrl {set;get;}
+        public string? LinkedInUrl {set;get;}
+        public string ImageUrl {set;get;}
+        public string? Description {set;get;}
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public bool SoftDelete {set;get;}       
+    }
+
+}
+
