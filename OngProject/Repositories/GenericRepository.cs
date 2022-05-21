3989c43dda1f4679e97806445f91ae5252becbb2
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-05-20 22:53:37 -0300

    repositories

---
+++ OngProject/Repositories/GenericRepository.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OngProject.DataAccess;
+using OngProject.Repositories.Interfaces;
+
+namespace OngProject.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private OngProjectDbContext _ongProjectDbContext;
+        
+        public GenericRepository(OngProjectDbContext ongProjectDbContext)
+        {
+            this._ongProjectDbContext = ongProjectDbContext;
+        }
+        public async Task Delete(int id)
+        {
+            var entity = await GetById(id);
+
+            if(entity == null)
+                throw new Exception("The entity is null");
+
+            _ongProjectDbContext.Set<TEntity>();//.Remove(entity);
+            await _ongProjectDbContext.SaveChangesAsync();
+        }
+
+        public async Task<List<TEntity>> GetAll()
+        {
+            return await _ongProjectDbContext.Set<TEntity>().ToListAsync();
+        }
+
+        public async Task<TEntity> GetById(int id)
+        {
+            return await _ongProjectDbContext.Set<TEntity>().FindAsync(id);
+        }
+
+        public async Task<TEntity> Insert(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            await _ongProjectDbContext.Set<TEntity>().AddAsync(entity);
+            await _ongProjectDbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<TEntity> Update(TEntity entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            _ongProjectDbContext.Set<TEntity>().Update(entity);
+            await _ongProjectDbContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+}
---
+++ OngProject/Repositories/MemberRepository.cs
@@ -0,0 +1,15 @@
+using OngProject.DataAccess;
+using OngProject.Entities;
+using OngProject.Repositories.Interfaces;
+
+namespace OngProject.Repositories
+{
+    public class MemberRepository : GenericRepository<Member>, IMemberRepository
+    {
+        public MemberRepository(OngProjectDbContext ongProjectDbContext) : base(ongProjectDbContext)
+        {            
+        }
+    }
+
+}
+
