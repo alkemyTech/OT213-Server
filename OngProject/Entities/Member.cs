61ba3c0bb0a24cf5ac2bad87b4273847b1e030ef
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-05-21 19:05:25 -0300

    updated changes

--- OngProject/Entities/Member.cs
+++ OngProject/Entities/Member.cs
@@ -8,14 +8,14 @@ namespace OngProject.Entities
         [Key]
         public int MembersID {set;get;}
         public string Name {set;get;}
-        public string? FacebookUrl {set;get;}
-        public string? InstagramUrl {set;get;}
-        public string? LinkedInUrl {set;get;}
+        public string FacebookUrl {set;get;}
+        public string InstagramUrl {set;get;}
+        public string LinkedInUrl {set;get;}
         public string ImageUrl {set;get;}
-        public string? Description {set;get;}
+        public string Description {set;get;}
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
-        public bool SoftDelete {set;get;}       
+        public bool isDeleted {set;get;}       
     }
 
 }
