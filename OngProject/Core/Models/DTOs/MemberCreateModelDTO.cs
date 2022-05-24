c8c7aec5e4c6a51aa674f9184afc746878608a33
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-05-24 00:37:13 -0300

    members dto's created

---
+++ OngProject/Core/Models/DTOs/MemberCreateModelDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Core.Models.DTOs
+{
+    public class MemberCreateModelDTO
+    {
+        public string name {set;get;}
+        public string facebookUrl {set;get;}
+        public string instagramUrl {set;get;}
+        public string linkedInUrl {set;get;}
+        public string imageUrl {set;get;}
+        public string description {set;get;}
+        public DateTime createdAt { get; set; } = DateTime.Now;
+        public DateTime updatedAt { get; set; } = DateTime.Now;
+    }
+}
