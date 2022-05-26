6f62d3ca49d6f944f75085e3c35602031218907e
xv13r <44001932+xv13r@users.noreply.github.com>
2022-05-26 00:19:55 -0300

    upload OT213-22

--- OngProject/Core/Mapper/EntityMapper.cs
+++ OngProject/Core/Mapper/EntityMapper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OngProject.Core.Models;
+using OngProject.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +9,30 @@ namespace OngProject.Core.Mapper
 {
     public class EntityMapper
     {
+        public Roles ToNewEntity(RoleModelDto roleDto)
+        {
+            return new Roles()
+            {
+                Name = roleDto.Name,
+            };
+        }
+
+        public Roles ToUpdateEntity(RoleModelDto roleDto)
+        {
+            return new Roles()
+            {
+                Id = roleDto.Id,
+                Name = roleDto.Name,
+                TimeStamp = DateTime.Now
+            };
+        }
+
+        public RoleModelDto ToModelDto(Roles role)
+        {
+            return new RoleModelDto()
+            {
+                Name = role.Name,
+            };
+        }
     }
 }
