e8c7e7513f6ede6ddfdd3977d64cc5c79f6bb4b7
xv13r <44001932+xv13r@users.noreply.github.com>
2022-05-26 00:23:29 -0300

    upload OT213-15

---
+++ OngProject/Entities/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OngProject.Entities
+{
+    /*
+     * timestamps y softDeletes
+     * Me parece que esta lo pide en todos los modelos, si les parece se podemos usar así
+     * public class <nombremodelo> : BaseModel
+     * 
+     */
+    public class BaseModel
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { set; get; }
+    }
+}
