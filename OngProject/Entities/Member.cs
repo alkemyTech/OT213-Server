107f475cc9e8c35742a913468d2cc7e3993a1dac
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-05-28 13:51:12 -0300

    Entities

--- OngProject/Entities/Member.cs
+++ OngProject/Entities/Member.cs
@@ -1,21 +1,13 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-
 namespace OngProject.Entities
 {
-    public class Member
+    public class Member : BaseEntity
     {
-        [Key]
-        public int MembersID {set;get;}
         public string Name {set;get;}
         public string FacebookUrl {set;get;}
         public string InstagramUrl {set;get;}
         public string LinkedInUrl {set;get;}
         public string ImageUrl {set;get;}
-        public string Description {set;get;}
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
-        public bool isDeleted {set;get;} = false;       
+        public string Description {set;get;}    
     }
 
 }
---
+++ OngProject/Entities/New.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class New : BaseEntity
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [Required]
+        public string Image { get; set; }      
+
+        // FK_Categories id
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
