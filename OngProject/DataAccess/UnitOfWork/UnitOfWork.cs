3829d2a1e068523a4e020c69b084d10df4fae96d
Macarena Zalazar <82382979+MacarenaZalazar@users.noreply.github.com>
2022-05-31 20:22:14 -0300

    unit of work updated

--- OngProject/DataAccess/UnitOfWork/UnitOfWork.cs
+++ OngProject/DataAccess/UnitOfWork/UnitOfWork.cs
@@ -1,24 +1,37 @@
 using System.Threading.Tasks;
 using OngProject.DataAccess.UnitOfWork.Interfaces;
 using OngProject.Repositories;
+using OngProject.Repositories.Auth;
+using OngProject.Repositories.Auth.Interfaces;
 using OngProject.Repositories.Interfaces;
 
 namespace OngProject.DataAccess.UnitOfWork
 {
     public class UnitOfWork : IUnitOfWork
     {
-        private readonly OngProjectDbContext _context;        
-        public IMemberRepository Members {get; private set;}
+        private readonly OngProjectDbContext _context;
+        public IMemberRepository Members { get; private set; }
         public IRoleRepository Roles { get; private set; }
 
+        public ITestimonialRepository Testimonials { get; private set; }
+        public IAuthRepository Authentications { get; private set; }
+        public IOrganizationRepository Organizations { get; private set; }
+        public IActivitiesRepository Activities { get; private set; }
         public ICategoriesRepository Categories { get; private set; }
-
         public UnitOfWork(OngProjectDbContext context)
         {
             this._context = context;
-            Members = new MemberRepository(_context);  
-            Roles = new RoleRepository(_context);
+            Members = new MemberRepository(_context);
+            Testimonials = new TestimonialRepository(_context);
+            Authentications = new AuthRepository(_context);
+            Organizations = new OrganizationRepository(_context);
+            Activities = new ActivitiesRepository(_context);
             Categories = new CategoriesRepository(_context);
+            /*
+                Error here because MemberBusiness doesn't implement a context class, instead MemberRepository
+            */
+
+            //Members2 = new MemberBusiness(_context);  
         }
 
         public async Task SaveAsync()
@@ -29,9 +42,8 @@ namespace OngProject.DataAccess.UnitOfWork
         public void Dispose()
         {
             _context.Dispose();
-        }    
+        }
 
     }
 
 }
-
