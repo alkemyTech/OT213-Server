35713379a187b609467a68bca7477306dd4b7c86
xv13r <44001932+xv13r@users.noreply.github.com>
2022-06-06 12:02:32 -0300

    updated 2022-06-06

--- OngProject/Core/Models/DTOs/Testimonial/TestimonialGetDTO.cs
+++ OngProject/Core/Models/DTOs/Testimonial/TestimonialGetDTO.cs
@@ -8,11 +8,6 @@ namespace OngProject.Core.Models.DTOs.Testimonial
         public string Name { get; set; }
         public string Image { get; set; }
         public string Description { get; set; }
-        public DateTime ModifiedAt { get; set; }
         public bool IsDeleted { get; set; }
-        public DateTime DeletedAt { get; set; }
-
     }
-
 }
-
