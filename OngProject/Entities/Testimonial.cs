41f7a9738f27d6590efda9a3e54fccbc93fb32d3
xv13r <44001932+xv13r@users.noreply.github.com>
2022-06-06 12:03:57 -0300

    updated 2022-06-06

--- OngProject/Entities/Testimonial.cs
+++ OngProject/Entities/Testimonial.cs
@@ -7,8 +7,5 @@ namespace OngProject.Entities
         public string Name { get; set; }
         public string Image { get; set; }
         public string Description { get; set; }
-        public DateTime ModifiedAt { get; set; }
-        public new bool IsDeleted { get; set; }
-        public DateTime DeletedAt { get; set; }
     }
 }
