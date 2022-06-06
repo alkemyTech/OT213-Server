7b0759d1ce0bba75d0bf1e694d88070914d71a9c
xv13r <44001932+xv13r@users.noreply.github.com>
2022-06-06 12:22:46 -0300

    updated 2022-06-06

---
+++ OngProject/DataAccess/Seeder/New/NewsConfiguration.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace OngProject.DataAccess.Seeder.New
+{
+    public class NewsConfiguration : IEntityTypeConfiguration<Entities.New>
+    {
+        public void Configure(EntityTypeBuilder<Entities.New> builder)
+        {
+            // Property Configurations 
+            builder.ToTable("News");
+            builder.Property(n => n.Id)
+                    .IsRequired();
+
+            builder.Property(n => n.Name)
+                    .HasMaxLength(50)
+                    .IsRequired();
+
+            builder.Property(n => n.Image)
+                    .IsRequired();
+
+            builder.Property(n => n.Content)
+                    .HasMaxLength(254);
+
+            // Populate the table Members
+            builder.HasData(
+                new Entities.New
+                {
+                    Id = 1,
+                    Name = "No news",
+                    Image = "https://www.vidimsoft.com/sites/default/files/styles/blog_lg/public/articles/nonewsyet.jpg?itok=51EKSfgX",
+                    Content = "No news, good news"
+                },
+                new Entities.New
+                {
+                    Id = 2,
+                    Name = "No news",
+                    Image = "https://www.vidimsoft.com/sites/default/files/styles/blog_lg/public/articles/nonewsyet.jpg?itok=51EKSfgX",
+                    Content = "No news, good news"
+                },
+                new Entities.New
+                {
+                    Id = 3,
+                    Name = "No news",
+                    Image = "https://www.vidimsoft.com/sites/default/files/styles/blog_lg/public/articles/nonewsyet.jpg?itok=51EKSfgX",
+                    Content = "No news, good news"
+                },
+                new Entities.New
+                {
+                    Id = 4,
+                    Name = "No news",
+                    Image = "https://www.vidimsoft.com/sites/default/files/styles/blog_lg/public/articles/nonewsyet.jpg?itok=51EKSfgX",
+                    Content = "No news, good news"
+                },
+                new Entities.New
+                {
+                    Id = 5,
+                    Name = "No news",
+                    Image = "https://www.vidimsoft.com/sites/default/files/styles/blog_lg/public/articles/nonewsyet.jpg?itok=51EKSfgX",
+                    Content = "No news, good news"
+                }
+            );
+        }
+    }
+
+
+}
+
