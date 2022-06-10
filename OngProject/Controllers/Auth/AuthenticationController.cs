707208b5f41c30ee9ab3394189614c86214c39e4
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-06-10 12:48:08 -0300

    Auth controller

--- OngProject/Controllers/Auth/AuthenticationController.cs
+++ OngProject/Controllers/Auth/AuthenticationController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OngProject.Core.Auth.Interfaces;
 using OngProject.Core.Business.Mail.Interfaces;
+using OngProject.Core.Helper.Interface;
 using OngProject.Core.Models.DTOs.Users.Auth;
 using OngProject.Entities;
 
@@ -14,18 +15,23 @@ namespace OngProject.Controllers
     {
         private readonly IAuthBusiness _authBusiness;
         private readonly IMailBusiness _mailBusiness;
+        private readonly ITokenService _tokenService;
         private readonly IMapper _mapper;
 
-        public AuthenticationController(IAuthBusiness authBusiness, IMailBusiness mailBusiness, IMapper mapper)
+        public AuthenticationController(IAuthBusiness authBusiness, 
+                                        IMailBusiness mailBusiness, 
+                                        IMapper mapper,
+                                        ITokenService token)
         {
             this._authBusiness = authBusiness;
             this._mailBusiness = mailBusiness;
             this._mapper = mapper;
+            this._tokenService = token;
         }
 
         [HttpPost]
         [Route("Auth/Register")]
-        public async Task<IActionResult> Register(UserRegisterModelDTO dto)
+        public async Task<IActionResult> Register([FromForm] UserRegisterModelDTO dto)
         {
             // validations
             dto.Email = dto.Email.ToLower();
@@ -39,14 +45,33 @@ namespace OngProject.Controllers
             }
 
             // request
-            await _authBusiness.Registrar(_mapper.Map<User>(dto), dto.Password);
+            var user = await _authBusiness.Registrar(_mapper.Map<User>(dto), dto.Password);
+            //var imgFormat = ver como convertir el IFormFile a url antes de mapear el usuario
+            var mappedUser = _mapper.Map<UserGetModelDTO>(user);
             await _mailBusiness.SendEmailAsync(dto.Email);
 
             return Ok(new 
             {
                 Status = "Success",
-                Message = "User creation successfully!"
+                Message = "User registered successfully!",
+                User = mappedUser
             }); 
+        }
+
+        [HttpPost]
+        [Route("Auth/Login")]
+        public async Task<IActionResult> Login(UserLoginModelDTO dto)
+        {
+            var user = await _authBusiness.Login(dto.Email, dto.Password);
+            if(user == null)
+                return Unauthorized();            
+                
+            var token = _tokenService.CreateToken(user);  
+
+            return Ok(new 
+            {
+                Token = token
+            });         
         }
 
     }
