e82dbaa3b067dabe692a59d1b6960cd1bc139388
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-06-22 12:06:34 -0300

    RolController Done

--- OngProject/Controllers/RoleController.cs
+++ OngProject/Controllers/RoleController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using OngProject.Core.Interfaces;
 using OngProject.Core.Mapper;
 using OngProject.Core.Models.DTOs.Roles;
-using OngProject.DataAccess.UnitOfWork.Interfaces;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace OngProject.Controllers
@@ -15,37 +15,44 @@ namespace OngProject.Controllers
     {
         private readonly IRoleBusiness _rolBusiness;
         private readonly IMapper _mapper;
-        private readonly IUnitOfWork _unitOfWork;
-        public RoleController(IUnitOfWork unitOfWork, IRoleBusiness rolBusiness, IMapper mapper)
+        public RoleController(IRoleBusiness rolBusiness, IMapper mapper)
         {
-            _unitOfWork = unitOfWork;
             this._rolBusiness = rolBusiness;
             this._mapper = mapper;
         }
 
-        [HttpGet(nameof(GetRoles))]
-        public async Task<IActionResult> GetRoles() => Ok(await _unitOfWork.Role.GetAll());
+        [HttpGet]
+        [Route("/Role/all")]
+        public  IActionResult GetAllRoles()
+        {
+            var role = _rolBusiness.Find(c => c.IsDeleted == false);
+            return Ok(_mapper.Map<IEnumerable<RoleGetDTO>>(role));
+        }
 
-        [HttpGet(nameof(GetRoleById))]
-        public async Task<IActionResult> GetRoleById([FromQuery] int roleID) => Ok(await _unitOfWork.Role.GetById(roleID));
+        [HttpGet]
+        [Route("/Role/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var role = await _rolBusiness.GetById(id);
+            return Ok(_mapper.Map<RoleGetDTO>(role));
+        }
 
         [HttpPost(nameof(CreateRole))]
-        public async Task<IActionResult> CreateRole(RoleModelDto role)
+        public async Task<IActionResult> CreateRole(RoleCreateDto role)
         {
-            var result = await _unitOfWork.Role.Insert(new EntityMapper().ToNewEntity(role));
-            await _unitOfWork.SaveAsync();
+            var result = await _rolBusiness.Insert(new EntityMapper().ToNewEntity(role));
             if (result is not null) 
-                return Ok("Role Created");
+                return Ok($"{role.Name} role created");
             else 
                 return BadRequest("Error in Creating the role");
         }
 
         [HttpPut(nameof(UpdateRole))]
-        public async Task<IActionResult> UpdateRole(int id, RoleModelDto dto)
+        public async Task<IActionResult> UpdateRole(int id, RoleUpdateDTO dto)
         {
             if (id != dto.Id)
             {
-                return StatusCode(StatusCodes.Status400BadRequest, new
+                return StatusCode(StatusCodes.Status404NotFound, new
                 {
                     Status = "Error",
                     Message = "Id number doesn't match!"
@@ -54,7 +61,7 @@ namespace OngProject.Controllers
 
             var rol = await _rolBusiness.GetById(id);
             _mapper.Map(dto, rol);
-            var updated = await _rolBusiness.Update(rol);            
+            await _rolBusiness.Update(rol);            
    
             return Ok(new 
             {
