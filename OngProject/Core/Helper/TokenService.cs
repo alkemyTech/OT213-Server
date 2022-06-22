eca86a1984e725429852a1623d17e70565a6a772
Guillermo Donalisio <guillermo.donalisio1@gmail.com>
2022-06-22 11:34:53 -0300

    setting role Claim

--- OngProject/Core/Helper/TokenService.cs
+++ OngProject/Core/Helper/TokenService.cs
@@ -24,7 +24,7 @@ namespace OngProject.Core.Helper
             var claim = new List<Claim>
             {
                 new Claim(ClaimTypes.Email, user.Email),
-                new Claim(ClaimTypes.Role, "Admin")
+                new Claim(ClaimTypes.Role, user.Role.Name)
             };
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Token"]));
